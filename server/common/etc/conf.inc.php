65ad8581ae1f0ceab2ef42da2d4b048d0872a77b
satanxm <satanxm@gmail.com>
2012-12-08 17:12:10 +0800

    add wordlist

---
+++ server/common/etc/conf.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DEFAULT_DOMAIN', 'ghost.com');
+
+// 域名白名单
+$_trust_domain = array(
+	'ghost.com',
+);
+
+?>
--- server/index.php
+++ server/index.php
@@ -6,37 +6,53 @@
  * $Dtime:2011-9-4
 ***********************************************************************************/
 
+
+$dir = (dirname(__FILE__));
+define('COMM_PATH', $dir . '/common/');
+define('ROOT_PATH', $dir . '/');
+unset($dir);
+
+require_once COMM_PATH . 'etc/conf.inc.php';
+//require_once COMM_PATH . 'lib/function.inc.php';
+require_once COMM_PATH . 'lib/charset.inc.php';
+require_once COMM_PATH . 'lib/json.inc.php';
+
 //require('include/conn.php');
 //$runtime= new runtime;
 //$runtime->start();
 
+// 模块动作识别
+if (empty($_REQUEST['mod']) || !preg_match('/^[a-z]{3,16}$/i', $_REQUEST['mod'])) {
+	$_REQUEST['mod'] = 'default';
+	$mod_name = 'default';
+} else {
+	$_REQUEST['mod'] = strtolower($_REQUEST['mod']);
+	$mod_name = $_REQUEST['mod'];
+}
+if (empty($_REQUEST['act']) || !preg_match('/^[a-z]{3,16}$/i', $_REQUEST['act'])) {
+	$_REQUEST['act'] = 'default';
+}
+$_REQUEST['act'] = strtolower($_REQUEST['act']);
+$act_name = $_REQUEST['act'];
 
-$request_url=$_SERVER["REQUEST_URI"];
-$request_url=str_replace("index.php?","",$request_url);
-$htdoc_root="api";
-
-$parm=str_replace("/".$htdoc_root."/","",$request_url); 
-
-$parm_arr=explode("_",$parm);
-$module=$parm_arr[0];
-$method=$parm_arr[1];
 
 $post=$_POST;
 foreach($post as $key=>$val) {
 	$post[$key]=addslashes($val);
 }
-
-
 /*require_once('module/user.php');
 $module_str="\$simulation=new userMod;";
 $method_str="\$simulation->cookie_query();";
 eval($module_str.$method_str);*/
 
 
+/*var_dump($module);
+var_dump($method);*/
+
 // 模块入口
-require_once('module/'.$module.'.php');
-$module_str="\$simulation=new ".$module."Mod;";
-$method_str="\$simulation->".$method."(\$post);";
+require_once('module/'.$mod_name.'.php');
+$module_str="\$simulation=new ".$mod_name."Mod;";
+$method_str="\$simulation->".$act_name."(\$post);";
 eval($module_str.$method_str);
 
 //$runtime->stop();
