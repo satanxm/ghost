d2c987ba72c4baf4d76aaf43ea3cd6c207fcf675
youkunhuang <345680969@qq.com>
2012-12-08 20:23:09 +0800

    js

--- js/module/winner/src/winner.js
+++ js/module/winner/src/winner.js
@@ -9,22 +9,28 @@ define(function(require,exports,module){
 		//胜利者 1:人 2:鬼
 		showWinner : function(data){
 			
-			debugger;
-			
-			if(data.identity === 1){
+			if(userInfo.identity === 1){
 				
 				if(data.winner === 1){
+					
 					this.displayWinner();
+					
 				}else if(data.winner === 2){
+					
 					this.displayLoser();
+					
 				}
 				
-			}else if(data.identity === 2){
+			}else if(userInfo.identity === 2){
 				
 				if(data.winner === 1){
+					
 					this.displayLoser();
+					
 				}else if(data.winner === 2){
+					
 					this.displayWinner();
+					
 				}
 			}
 			
