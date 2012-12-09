51b3b51788aa6cebda6b572b899e66f85b6af00b
youkunhuang <345680969@qq.com>
2012-12-09 12:12:14 +0800

    voteanimate

---
+++ js/module/vote/easing.js
@@ -0,0 +1,13 @@
+jQuery.extend( jQuery.easing,
+{
+	easeInSine: function (x, t, b, c, d) {
+		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+	},
+	easeOutSine: function (x, t, b, c, d) {
+		return c * Math.sin(t/d * (Math.PI/2)) + b;
+	}
+});
+
+define(function(){
+	return jQuery;
+});
--- js/module/vote/result.js
+++ js/module/vote/result.js
@@ -4,6 +4,7 @@
 define(function(require,exports,module){
 
 	var centerTips = require('ghost.v1/api/centerTips');
+	require('./easing');
 
 	return {
 		
@@ -22,6 +23,79 @@ define(function(require,exports,module){
 		            voteNumNode.removeClass('voteNum_voted')
 		        }
 		    });
+			
+		},
+		
+		/**
+		 * 
+		 * 显示投票动画
+		 * 
+		 */
+		showAnimate: function(data){
+			
+			data = data || {};
+			data.animateList = [
+				{
+					from: 0,
+					to: 3
+				},
+				{
+					from: 0,
+					to: 6
+				},
+				{
+					from: 0,
+					to: 10
+				},
+				{
+					from: 0,
+					to: 13
+				},
+				{
+					from: 0,
+					to: 4
+				}
+			];
+			
+			var desk = $('#page_main .mod_desk');
+			
+			desk.stop(true,true);
+			
+			$.each(data.animateList,function(i,v){
+				
+				var from = desk.find('.inner .user_pos_' + v.from);
+				var to = desk.find('.inner .user_pos_' + v.to);
+				var curr;
+				
+				if(from.size() === 0 || to.size() === 0){
+					return;
+				}
+				
+				desk.queue(function(){
+					curr = from.clone();
+					curr.appendTo(desk);
+					curr.css({
+						'transform': 'scale(0.5)',
+						'-webkit-transform': 'scale(0.5)',
+						'-moz-transform': 'scale(0.5)',
+						'-o-transform': 'scale(0.5)',
+						zIndex: 99999
+					});
+					//from.hide();
+					curr.animate({
+						top: [to.offset().top - desk.offset().top,'easeOutSine'],
+						left: [to.offset().left - desk.offset().left,'easeInSine']
+					},600,function(){
+						curr.remove();
+						from.show();
+						desk.dequeue();
+					});
+				});
+				
+			});
+			
+			
+			
 			
 		}
 		
